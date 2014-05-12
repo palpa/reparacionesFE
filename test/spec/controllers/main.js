c28b1685178c33a9c4a6e648012cc9a1d5aa05cf
Damian Palpacelli <damian.palpacelli@gmail.com>
2014-05-11 21:14:06 -0300

    refactor(MainCtrlTest): inject $rootScope is not necessary

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -9,11 +9,9 @@ describe('Controller: MainCtrl', function () {
     scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
-    });
+  beforeEach(inject(function($controller) {
+    scope = {};
+    MainCtrl = $controller('MainCtrl', {$scope:scope});
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
