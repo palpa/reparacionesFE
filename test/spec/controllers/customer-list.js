399f9c3cc227f1e361f1c4658784db43d92b6abc
Damian Palpacelli <damian.palpacelli@gmail.com>
2014-05-19 23:55:54 -0300

    test(CustomerListCtrl): test for retrieved customers using a mocked promise

--- test/spec/controllers/customer-list.js
+++ test/spec/controllers/customer-list.js
@@ -5,16 +5,39 @@ describe('Controller: CustomerListCtrl', function () {
   // load the controller's module
   beforeEach(module('reparacionesFeApp'));
 
-  var CustomerListCtrl,
+  var CustomerListCtrl, CustomerService,
     scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    CustomerListCtrl = $controller('CustomerListCtrl', {
-      $scope: scope
+  var mockCustomerResource = [
+    {
+      id: 123,
+      name: 'Juan Mendoza'
+    },
+    {
+      id: 124,
+      name: 'Damian Palpacelli'
+    }
+  ];
+
+  beforeEach(function () {
+    module(function ($provide) {
+      CustomerService = jasmine.createSpyObj('CustomerService', ['query']);
+      $provide.value('CustomerService', CustomerService);
     });
-  }));
+
+    inject(function ($controller, $rootScope, _$q_) {
+
+      var customerListDeferred = _$q_.defer();
+      customerListDeferred.resolve(mockCustomerResource);
+      CustomerService.query.andReturn(customerListDeferred.promise);
+
+      // Initialize the controller and a mock scope
+      scope = $rootScope.$new();
+      CustomerListCtrl = $controller('CustomerListCtrl', {
+        $scope: scope
+      });
+    });
+  });
 
   it('should be defined', function () {
     expect(CustomerListCtrl).toBeDefined();
@@ -28,6 +51,23 @@ describe('Controller: CustomerListCtrl', function () {
     expect(scope.orderProp).toBe('name');
   });
 
-  it('should create "customers" model with 2 customers fetched from xhr', function () {
+  describe('should create a customers model that', function () {
+    beforeEach(function () {
+      scope.$apply();
+    });
+
+    it('should contain 2 customers', function () {
+      expect(scope.customers.length).toEqual(2);
+    });
+
+    it('should have first customer with id "123" and name "Juan Mendoza"', function () {
+      expect(scope.customers[0].id).toEqual(123);
+      expect(scope.customers[0].name).toEqual('Juan Mendoza');
+    });
+
+    it('should have second customer with id "124" and name "Damian Palpacelli"', function () {
+      expect(scope.customers[1].id).toEqual(124);
+      expect(scope.customers[1].name).toEqual('Damian Palpacelli');
+    });
   });
 });
